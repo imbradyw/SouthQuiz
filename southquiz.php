9903ce45fdc859f03f6468765b27850e9151be9d
imbradyw <i.am.brady.ward@gmail.com>
2020-01-17 16:58:47 -0500

    Push

--- southquiz.php
+++ southquiz.php
@@ -1,27 +1,20 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
  * @link              http://example.com
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           SouthQuiz
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Plugin Name:       SouthQuiz
+ * Plugin URI:        http://bradyward.ca
+ * Description:       A simple quiz plugin.
+ * Version:           0.0.1
+ * Author:            Frederic Brady Ward
+ * Author URI:        http://bradyward.ca
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       southquiz
  * Domain Path:       /languages
  */
 
@@ -30,39 +23,34 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define( 'SOUTHQUIZ_VERSION', '0.0.1' );
 
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-plugin-name-activator.php
  */
-function activate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
-	Plugin_Name_Activator::activate();
+function activate_southquiz() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-southquiz-activator.php';
+	SouthQuiz_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-plugin-name-deactivator.php
  */
-function deactivate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
-	Plugin_Name_Deactivator::deactivate();
+function deactivate_southquiz() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-southquiz-deactivator.php';
+	SouthQuiz_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_plugin_name' );
-register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+register_activation_hook( __FILE__, 'activate_southquiz' );
+register_deactivation_hook( __FILE__, 'deactivate_southquiz' );
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-southquiz.php';
 
 /**
  * Begins execution of the plugin.
@@ -73,10 +61,10 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
  *
  * @since    1.0.0
  */
-function run_plugin_name() {
+function run_southquiz() {
 
-	$plugin = new Plugin_Name();
+	$plugin = new SouthQuiz();
 	$plugin->run();
 
 }
-run_plugin_name();
+run_southquiz();
